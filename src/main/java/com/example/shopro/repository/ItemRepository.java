230c49507872817f57b85b4eb55393df39cbf2ab
simply <simply9507@naver.con>
2024-11-18 14:15:15 +0900

    쇼핑물

--- src/main/java/com/example/shopro/repository/ItemRepository.java
+++ src/main/java/com/example/shopro/repository/ItemRepository.java
@@ -2,8 +2,10 @@ package com.example.shopro.repository;
 
 import com.example.shopro.endtity.Item;
 import com.example.shopro.endtity.Item;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -27,9 +29,26 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     //가격으로 검색
     public List<Item> findByPriceLessThan(Integer price);
+    public List<Item> findByPriceGreaterThan(Integer price);
+    public List<Item> findByPriceGreaterThanOrderByPriceAsc(Integer price);
+
+    //페이징처리된 초과이면서 같은거
+    public List<Item> findByPriceGreaterThanEqual(Integer price, Pageable pageable);
+
 
     //정렬까지 추가
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    //nativeQuery 사용
+    @Query(value = "select * from Item where i.itemNm = :itemNm", nativeQuery = true)
+    List<Item> nativeQueryselectwhereNamelike(@Param("itemNm") String itemNm, Pageable pageable);
+
+    //생성날짜
+    //판매 많은 순
+    //별점 높은 순
+    //등록일순
+    //~금액 이하
+    //
 
 
 
--- src/test/java/com/example/shopro/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/shopro/repository/ItemRepositoryTest.java
@@ -2,11 +2,25 @@ package com.example.shopro.repository;
 
 import com.example.shopro.constant.ItemSellStatus;
 import com.example.shopro.endtity.Item;
+import com.example.shopro.endtity.QItem;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.QueryFactory;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.extern.log4j.Log4j2;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -20,6 +34,9 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager entityManager;
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -73,5 +90,122 @@ class ItemRepositoryTest {
         System.out.println("------------------------------");
 
     }
+    @Test
+    public void priceSearchtest(){
+        //가격 검색 테스트
+        //사용자가 검색창에 혹은 검색이 가능하도록 만들어 둔 곳에 값을 입력한다는 조건
+        //위 조건에 부합하는 아이템 리스트 검색
+        Integer price = 10020;
+
+        List<Item> itemList =
+        itemRepository.findByPriceLessThan(price);
+        for ( Item item:itemList){
+            log.info(item);
+            log.info("상품명 : "+item.getItemNm());
+            log.info("상품가격 : "+item.getPrice());
+            log.info("상품상세설명 : "+item.getItemDetail());
+
+        }
+
+        List<Item> itemListA =
+                itemRepository.findByPriceGreaterThan(price);
+        for ( Item item:itemListA){
+            log.info(item);
+            log.info("상품명 : "+item.getItemNm());
+            log.info("상품가격 : "+item.getPrice());
+            log.info("상품상세설명 : "+item.getItemDetail());
+
+        } List<Item> itemListB =
+                itemRepository.findByPriceGreaterThanOrderByPriceAsc(price);
+        for ( Item item:itemListB){
+            log.info(item);
+            log.info("상품명 : "+item.getItemNm());
+            log.info("상품가격 : "+item.getPrice());
+            log.info("상품상세설명 : "+item.getItemDetail());
+
+        }
+    }
+    @Test
+    @DisplayName("페이징 추가까지")
+    public void findbypricegreaterThanEqualTest(){
+        Pageable pageable = PageRequest
+                .of(0,5, Sort.by("id").ascending());
+                                                    //sort.bu의 정렬조건은 entity의 변수명이다.
+
+        Integer price = 10020;
+        List<Item> itemList =
+        itemRepository.findByPriceGreaterThanEqual(price, pageable);
+
+
+        itemList.forEach(item -> log.info(item));
+
+    }
+    @Test
+    public void nativeQueryTest(){
+
+        Pageable pageable =
+                PageRequest.of(0,5, Sort.by("price").descending());
+        String itemNm = "테스트상품1";
+        List<Item> itemList =
+                itemRepository.nativeQueryselectwhereNamelike(itemNm, pageable);
+        itemList.forEach((item -> log.info(item)));
+    }
+
+
+    @Test
+    public void queryDslTest(){
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+
+        QItem qItem = QItem.item;
+        //select *from item
+        String keyword = null;
+        ItemSellStatus itemSellStatus = ItemSellStatus.SELL;
+        JPAQuery<Item> query =
+                queryFactory.selectFrom(qItem)
+                        .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                        .where(qItem.itemDetail.like("%"+"1"+"%"))
+                        .orderBy(qItem.price.desc());
+        List<Item>itemList = query.fetch();
+
+        for (Item item : itemList){
+            System.out.println(item.getItemNm());
+        }
+
+    }
+    @Test
+    public void queryDslTestB(){
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+
+        QItem qItem = QItem.item;
+        //select *from item
+        //상품검색 조건 입력값
+        String keyword = null;
+        ItemSellStatus itemSellStatus = ItemSellStatus.SELL;
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        if (keyword != null){
+            booleanBuilder.and(qItem.itemDetail.like("%"+keyword+"%"));
+        }
+        if (itemSellStatus != null) {
+            if (itemSellStatus == ItemSellStatus.SELL) {
+                booleanBuilder.or(qItem.itemSellStatus.eq(ItemSellStatus.SELL));
+            } else {
+                booleanBuilder.or(qItem.itemSellStatus.eq(ItemSellStatus.SOLD_OUT));
+            }
+        }
+
+        JPAQuery<Item> query =
+                queryFactory.selectFrom(qItem)
+                        .where(booleanBuilder)
+                        .orderBy(qItem.price.desc());
+        List<Item>itemList = query.fetch();
+
+        for (Item item : itemList){
+            System.out.println(item.getItemNm());
+        }
+
+    }
+
+
 
 }
